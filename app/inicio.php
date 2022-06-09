a1d9a763ce793ebbcd7ae4344c2b9c8eebe6b092
zyz7 <zyz7@pm.me>
2022-06-09 14:33:21 +0000

    app

--- app/inicio.php
+++ app/inicio.php
@@ -1,22 +1,16 @@
 <?php
 
 /*
- * \date 2021
- * \author Mario Alberto Zayas González
- */
-
-/*
+ * Autor: Mario Alberto Zayas González
+ * Fecha: Junio 2022
  * Define la constante RUTA
  * En modo local se cambia por el nombre de la carpeta
  * Ejemplo: /nombre_carpeta/
  */
+
 define('RUTA', '/');
 
-/// Si no se pueden incluir los siguientes archivos se detiene la ejecución
+/// Si no se pueden incluir los siguientes archivos no se ejecuta
 require_once('librerias/MysqlConexion.php');
 require_once('librerias/Controlador.php');
 require_once('librerias/ControlUrl.php');
-require_once('librerias/Validar.php');
-require_once('librerias/Exception.php');
-require_once('librerias/PHPMailer.php');
-require_once('librerias/SMTP.php');
