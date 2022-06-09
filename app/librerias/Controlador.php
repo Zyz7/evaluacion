0a6c0ae15b60c48194c58307b04b9b15205a825d
zyz7 <zyz7@pm.me>
2022-06-09 14:23:28 +0000

    librerias

---
+++ app/librerias/Controlador.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * \class Controlador
+ * \brief Instancia el modelo e imprime la vista
+ * \date 2021
+ * \author Mario Alberto Zayas González
+ */
+class Controlador
+{
+  function __construct()
+  {
+  }
+
+  /// \fn modelo Instancia el modelo
+  public function modelo($modelo)
+  {
+	  require_once('../app/modelos/'.$modelo.'.php');
+	  return new $modelo();
+  }
+
+  /// \fn vista Imprime la vista con sus parámetros
+  public function vista($vista, $datos=[])
+  {
+    if (file_exists('../app/vistas/'.$vista.'.html')) {
+	    $template = file_get_contents('../app/vistas/'.$vista.'.html');
+
+	    /// Sustituye los parámetros por su variable con la forma {valor}
+      foreach ($datos as $clave => $valor) {
+        $template = str_replace('{'.$clave.'}', $valor, $template);
+      }
+      print $template;
+	  } elseif (file_exists('../app/vistas/'.$vista.'.php')) {
+      require_once("../app/vistas/".$vista.".php");
+    } else {
+	    die('La vista no existe');
+	  }
+  }
+
+}
